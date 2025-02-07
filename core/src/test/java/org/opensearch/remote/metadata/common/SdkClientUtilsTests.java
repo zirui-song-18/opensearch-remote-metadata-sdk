f59212a2912d3f0a00c57bf8d4de718ac0b7bb42
Daniel Widdis <widdis@gmail.com>
2025-02-07 13:36:57 -0800

    Improve exception unwrapping flexibility for SdkClientUtils (#67)
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- core/src/test/java/org/opensearch/remote/metadata/common/SdkClientUtilsTests.java
+++ core/src/test/java/org/opensearch/remote/metadata/common/SdkClientUtilsTests.java
@@ -8,6 +8,7 @@
  */
 package org.opensearch.remote.metadata.common;
 
+import org.opensearch.OpenSearchException;
 import org.opensearch.OpenSearchStatusException;
 import org.opensearch.action.support.PlainActionFuture;
 import org.opensearch.core.rest.RestStatus;
@@ -67,6 +68,61 @@ public class SdkClientUtilsTests {
 
         e = SdkClientUtils.unwrapAndConvertToException(ioException);
         assertSame(ioException, e);
+    }
+
+    @Test
+    public void testUnwrapAndConvertToException_VarargsUnwrap() {
+        // Create nested exceptions
+        OpenSearchException openSearchException = new OpenSearchException("Custom exception");
+        CompletionException completionException = new CompletionException(openSearchException);
+        OpenSearchStatusException statusException = new OpenSearchStatusException(
+            "Status exception",
+            RestStatus.INTERNAL_SERVER_ERROR,
+            completionException
+        );
+
+        // Test unwrapping with multiple exception types
+        Exception result = SdkClientUtils.unwrapAndConvertToException(
+            statusException,
+            OpenSearchStatusException.class,
+            CompletionException.class,
+            OpenSearchException.class
+        );
+        assertSame(openSearchException, result, "Should unwrap to the OpenSearchException");
+
+        // Test with a different order of exception types (order shouldn't matter now)
+        result = SdkClientUtils.unwrapAndConvertToException(
+            statusException,
+            CompletionException.class,
+            OpenSearchException.class,
+            OpenSearchStatusException.class
+        );
+        assertSame(openSearchException, result, "Should still unwrap to the OpenSearchException regardless of order");
+
+        // Test with only one exception type
+        result = SdkClientUtils.unwrapAndConvertToException(statusException, OpenSearchStatusException.class);
+        assertSame(completionException, result, "Should unwrap to the CompletionException (cause of OpenSearchStatusException)");
+
+        // Test with no matching exception type
+        IOException ioException = new IOException("IO Exception");
+        result = SdkClientUtils.unwrapAndConvertToException(ioException, OpenSearchException.class, CompletionException.class);
+        assertSame(ioException, result, "Should return the original exception when no matching type is found");
+
+        // Test with default behavior (only CompletionException)
+        result = SdkClientUtils.unwrapAndConvertToException(completionException);
+        assertSame(openSearchException, result, "Should unwrap CompletionException by default");
+
+        // Test with InterruptedException
+        InterruptedException interruptedException = new InterruptedException("Interrupted");
+        result = SdkClientUtils.unwrapAndConvertToException(interruptedException);
+        assertSame(interruptedException, result, "Should return InterruptedException and set interrupt flag");
+        assertTrue(Thread.interrupted(), "Interrupt flag should be set");
+
+        // Test with a non-Exception Throwable
+        Error error = new Error("Some error");
+        result = SdkClientUtils.unwrapAndConvertToException(error);
+        assertTrue(result instanceof OpenSearchException, "Should wrap non-Exception Throwable in OpenSearchException");
+        assertSame(error, result.getCause(), "Wrapped OpenSearchException should have original Error as cause");
     }
 
     @Test
